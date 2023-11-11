8419b8940c22b4538893cece395ea265050ff7a5
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2023-11-10 20:15:59 -0500

    review two crystall balls question

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,18 @@
-export default function two_crystal_balls(breaks: boolean[]): number {
+const two_crystal_balls = (breaks: boolean[]): number => {
+  const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+  let i = jumpAmount;
 
-}
+  for (; i < breaks.length; i += jumpAmount) {
+    if (breaks[i]) break;
+  }
+
+  i -= jumpAmount;
+
+  for (let j = i; j < breaks.length; j++) {
+    if (breaks[j]) return j;
+  }
+
+  return -1;
+};
+
+export default two_crystal_balls;
