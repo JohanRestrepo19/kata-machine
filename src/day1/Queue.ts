e2c32a3ba8986098f78147659c6aad8c0dfc8df8
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2023-11-13 21:32:27 -0500

    implement Queues

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,45 @@
+type Node<T> = {
+  value: T;
+  next?: Node<T>;
+};
+
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
-    
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    const newNode: Node<T> = { value: item };
+    this.length++;
+
+    if (!this.tail) {
+      this.tail = this.head = newNode;
+      return;
     }
 
-    enqueue(item: T): void {
+    this.tail.next = newNode;
+    this.tail = newNode;
+  }
 
-}
-    deque(): T | undefined {
+  deque(): T | undefined {
+    if (!this.head) return undefined;
 
-}
-    peek(): T | undefined {
+    this.length--;
 
-}
+    const head = this.head;
+    this.head = this.head.next;
+
+    if(this.length === 0) this.tail = undefined
+
+    return head.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
