7a7029a1b95fa73d651f8a19ac886a04d443cbe7
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2023-11-27 21:47:52 -0500

    implement Quick Sort

--- src/__tests__/QuickSort.ts
+++ src/__tests__/QuickSort.ts
@@ -1,9 +1,11 @@
 import quick_sort from "@code/QuickSort";
 
-test("quick-sort", function () {
+test("quick-sort", function() {
   const arr = [9, 3, 7, 4, 69, 420, 42];
+  const arr2 = [7, 2, 3, 4, 5, 8, 20, 13, 11, 1, 5];
 
-  debugger;
   quick_sort(arr);
+  quick_sort(arr2);
   expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
+  expect(arr2).toEqual([1, 2, 3, 4, 5, 5, 7, 8, 11, 13, 20]);
 });
