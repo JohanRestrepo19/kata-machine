198303614afc6defbba680b0bbe5326b0ba976cd
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2023-12-20 11:41:59 -0500

    implement DFS on Binary Search Tree

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,15 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+const search = (
+  current: BinaryNode<number> | null,
+  needle: number,
+): boolean => {
+  if (!current) return false;
+  if (current.value === needle) return true;
+  if (needle < current.value) return search(current.left, needle);
+  return search(current.right, needle);
+};
 
-}
+const dfs = (head: BinaryNode<number>, needle: number): boolean => {
+  return search(head, needle);
+};
+
+export default dfs;
